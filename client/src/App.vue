f68c77b3caca89ec1424942362ef97ee04bb92c8
Zuk Levinson <zuklevinson@gmail.com>
2021-04-10 20:55:31 +0300

    feat(): page design setup and navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,85 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div flex col id="app">
+    <navbar />
+    <router-view />
   </div>
 </template>
 
-<style>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Navbar from "@/components/Navbar.vue";
+
+@Component({
+  components: {
+    Navbar,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+html,
+body {
+  margin: 0;
+}
+
+[navbar] {
+  min-height: 5vh;
+  top: 0;
+  box-shadow: 0px 0 7px rgb(206, 206, 206);
+}
+
+[flex] {
+  display: flex;
+
+  &[row] {
+    flex-direction: row;
+  }
+  &[col] {
+    flex-direction: column;
+  }
+
+  & > * {
+    flex: 12 12 auto;
+  }
+}
+
+[fill-width] {
+  width: 100%;
+}
+
+[fill-height] {
+  height: 100%;
+}
+
+[no-flex] {
+  flex: 0 !important;
+}
+
+#app {
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  background-color: #f4f4f4;
+  font-family: "Noto Sans JP", sans-serif;
+}
+
+[lg-3] {
+  width: 25%;
+  flex: 3;
+}
+
+h1 {
+  font-size: 1.5em;
+  margin: auto;
+  text-align: center;
+}
+
+h3 {
+  font-size: 1em;
+  margin: auto;
+  text-align: center;
+}
+
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap");
 </style>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div navbar no-flex flex row>
+    <div id="logo" lg-3 flex row>
+      <h1>Word Complete</h1>
+    </div>
+    <div id="information"></div>
+    <div id="links" lg-3 flex row>
+      <router-link to="/"><h3>Home</h3></router-link>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class Navbar extends Vue {}
+</script>
+<style lang="scss" scoped>
+a {
+  text-decoration: none;
+  display: flex;
+  color: black;
+}
+.router-link-exact-active {
+  color: gray;
+  text-decoration: underline;
+}
+</style>
