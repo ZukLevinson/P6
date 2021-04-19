a89cef6720b1bc25c92e0978d2a7906c864837b0
Zuk Levinson <zuklevinson@gmail.com>
2021-04-19 17:39:53 +0300

    fix(rnn.py): fixed issues with input order

--- ml/file_reader.py
+++ ml/file_reader.py
@@ -17,35 +17,76 @@ class FileReader:
         for (dir_path, dir_name, file_names) in os.walk(self.ROOT_FOLDER):
             if file_names:
                 for file in file_names:
-                    self.files[os.path.join(dir_path, file)] = []
+                    self.files[os.path.join(dir_path, file)] = 2
 
-    def read_random_lines(self, lines_count=2000):
+    # def read_random_lines(self, lines_count=2000):
+    #     lines = []
+    #     total_lines = 0
+    #
+    #     for file_path in self.files:
+    #         with io.open(file_path, newline='', encoding="utf-8") as file:
+    #             csv_reader = csv.reader(file)
+    #             total_lines += len(list(csv_reader))
+    #
+    #     for file_path in self.files:
+    #         with io.open(file_path, newline='', encoding="utf-8") as file:
+    #             csv_reader = csv.reader(file)
+    #
+    #             file_lines = list(csv_reader)
+    #
+    #             lines_in_file = len(file_lines)
+    #             lines_to_count = int((lines_in_file / total_lines) * lines_count)
+    #
+    #             for i in range(lines_to_count):
+    #                 while True:
+    #                     index = random.randint(0, lines_in_file - 1)
+    #
+    #                     if index not in self.files.get(file_path):
+    #                         break
+    #
+    #                 self.files[file_path].append(index)
+    #                 lines.append(file_lines[index])
+    #
+    #     random.shuffle(lines)
+    #     return lines
+
+    def get_lines(self, lines_count=2000):
         lines = []
-        total_lines = 0
+        line_total = 0
 
         for file_path in self.files:
             with io.open(file_path, newline='', encoding="utf-8") as file:
-                csv_reader = csv.reader(file)
-                total_lines += len(list(csv_reader))
+                if line_total == lines_count:
+                    return lines
 
-        for file_path in self.files:
-            with io.open(file_path, newline='', encoding="utf-8") as file:
-                csv_reader = csv.reader(file)
+                file_lines = list(csv.reader(file))
+                file_length = len(file_lines)
 
-                file_lines = list(csv_reader)
+                min_index = self.files.get(file_path)
 
-                lines_in_file = len(file_lines)
-                lines_to_count = int((lines_in_file / total_lines) * lines_count)
+                if min_index > file_length:
+                    continue
 
-                for i in range(lines_to_count):
-                    while True:
-                        index = random.randint(0, lines_in_file - 1)
+                max_index = min(file_length - 1, min_index + lines_count)
 
-                        if index not in self.files.get(file_path):
-                            break
+                lines += file_lines[min_index:max_index]
+                line_total += max_index - min_index
 
-                    self.files[file_path].append(index)
-                    lines.append(file_lines[index])
+                self.files[file_path] = max_index
 
-        random.shuffle(lines)
+                # print(f"MIN: {min_index}, MAX: {max_index}, FILE: {file_path}")
+
         return lines
+
+    def vocabulary(self, tokenizer):
+        for file_path in self.files:
+            with io.open(file_path, newline='', encoding="utf-8") as file:
+                csv_reader = csv.reader(file)
+                file_lines = [" ".join(line) for line in list(csv_reader)]
+
+                tokenizer.fit_on_texts([line.split() for line in file_lines])
+
+        result = len(tokenizer.word_index) + 1
+
+        print(f"Model vocabulary size is {result}")
+        return result
--- ml/rnn.py
+++ ml/rnn.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import time
 
 from wiki_reader import WikiReader, format_content
 from csv_writer import CSVWriter
@@ -18,62 +19,74 @@ class WordPredictor(Sequential):
     def __init__(self, num_of_words, **kwargs):
         super(WordPredictor, self).__init__()
 
+        reader = FileReader()
+
         self.NUMBER_OF_WORDS = num_of_words
-        self.VOCABULARY_SIZE = 50000
 
         self.tokenizer = Tokenizer()
 
+        self.VOCABULARY_SIZE = reader.vocabulary(self.tokenizer)
+
+
         # NETWORK
+        # self.add(Embedding(input_dim=self.VOCABULARY_SIZE, output_dim=50, input_length=self.NUMBER_OF_WORDS))
+        # self.add(LSTM(256, return_sequences=True))
+        # self.add(Dropout(0.2))
+        # self.add(LSTM(256))
+        # self.add(Dropout(0.2))
+        # self.add(Dense(self.VOCABULARY_SIZE, activation='softmax'))
         self.add(Embedding(input_dim=self.VOCABULARY_SIZE, output_dim=50, input_length=self.NUMBER_OF_WORDS))
-        self.add(LSTM(256, return_sequences=True))
-        self.add(Dropout(0.2))
-        self.add(LSTM(256))
-        self.add(Dropout(0.2))
+        self.add(LSTM(100, return_sequences=True))
+        self.add(LSTM(100))
+        self.add(Dense(100, activation='relu'))
         self.add(Dense(self.VOCABULARY_SIZE, activation='softmax'))
 
-        self.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        self.compile(loss='categorical_crossentropy', optimizer='adam',
+                     metrics=['accuracy', tf.keras.metrics.MeanAbsoluteError()])
 
-    def train_on_wiki(self):
-        self.fit(create_pre_process_generator(256, self.tokenizer, self.VOCABULARY_SIZE), epochs=100,
-                 steps_per_epoch=256)
+    def train_on_wiki(self, epochs=100, steps_per_epoch=10):
+        self.fit(create_pre_process_generator(epochs * steps_per_epoch, self.tokenizer, self.VOCABULARY_SIZE,
+                                              line_count=1),
+                 epochs=epochs,
+                 steps_per_epoch=steps_per_epoch)
+        self.save_weights(f"weights/rnn-{time.time()}")
+
+    def load_model(self):
+        self.load_weights("weights/rnn-1618827756.9249594")
+
+    def predict_words(self, text_seq_length, seed_text, n_words):
+        text = []
+
+        for _ in range(n_words):
+            encoded = self.tokenizer.texts_to_sequences([seed_text])[0]
+            encoded = pad_sequences([encoded], maxlen=text_seq_length, truncating='pre')
+
+            y_predict = np.argmax(self.predict(encoded), axis=-1)
+
+            predicted_word = ''
+            for word, index in self.tokenizer.word_index.items():
+                if index == y_predict:
+                    predicted_word = word
+                    break
+            seed_text = seed_text + ' ' + predicted_word
+            text.append(predicted_word)
+        return ' '.join(text)
 
 
-def create_pre_process_generator(num_of_sets, tokenizer, vocab_size, line_count=2000):
+def create_pre_process_generator(num_of_sets, tokenizer, vocab_size, line_count=1):
     reader = FileReader()
 
     for i in range(num_of_sets):
-        wiki_set = [' '.join(line) for line in reader.read_random_lines(line_count)]
-        tokenizer.fit_on_texts([format_content(line) for line in wiki_set])
-        sequence = np.array([token_list for token_list in tokenizer.texts_to_sequences([line for line in wiki_set]) if
-                             len(token_list) == 20])
+        wiki_set = [' '.join(line) for line in reader.get_lines(line_count)]
+        sequence = np.array([token_list for token_list in tokenizer.texts_to_sequences(wiki_set)])
 
-        # x, y = sequence[:, :-1], sequence[:, -1]
         x, y = np.asarray([line[:-1] for line in sequence], dtype=np.int32), [line[-1] for line in sequence]
         y = to_categorical(y, num_classes=vocab_size)
 
         yield x, y
 
 
-def predict_words(tf_model, tokenizer, text_seq_length, seed_text, n_words):
-    text = []
-
-    for _ in range(n_words):
-        encoded = tokenizer.texts_to_sequences([seed_text])[0]
-        encoded = pad_sequences([encoded], maxlen=text_seq_length, truncating='pre')
-
-        y_predict = np.argmax(tf_model.predict(encoded), axis=-1)
-
-        predicted_word = ''
-        for word, index in tokenizer.word_index.items():
-            if index == y_predict:
-                predicted_word = word
-                break
-        seed_text = seed_text + ' ' + predicted_word
-        text.append(predicted_word)
-    return ' '.join(text)
-
-
-def get_lines_separated(tokens, sentence_length=20):
+def get_lines_separated(tokens, sentence_length=50):
     for index in range(sentence_length, len(tokens)):
         sequence = tokens[index - sentence_length:index]
 
@@ -83,7 +96,7 @@ def get_lines_separated(tokens, sentence_length=20):
         yield [sentence, target]
 
 
-def generate_sets(set_length=200, max_rows=10000, max_folders=1000):
+def generate_sets(set_length=200, max_rows=200000, max_folders=1000):
     writer = CSVWriter()
 
     folder_name = writer.create_folder()
@@ -106,9 +119,16 @@ def generate_sets(set_length=200, max_rows=10000, max_folders=1000):
 
 
 if __name__ == "__main__":
-    # # Run RNN
-    # rnn = WordPredictor(20)
-    # rnn.train_on_wiki()
+    rnn = WordPredictor(49)
+
+    # Train RNN
+    rnn.train_on_wiki(100, 256)
 
     # Generate new sets
-    generate_sets()
+    # generate_sets(200000)
+
+    # Load weights
+    # rnn.load_model()
+
+    # Predict
+    # print(rnn.predict_words(1, "hello", 10))
